8b5f962be76642c538e078b884a70b1aad00d9ed
NormanDev <kulavets@yandex.ru>
2026-03-16 14:31:25 +0400

    feat(directories): добавлена модель Unit (Единицы измерения)

--- apps/directories/models.py
+++ apps/directories/models.py
@@ -25,3 +25,28 @@ class WorkType(BaseModel):
     
     def __str__(self):
         return self.full_name
+    
+
+class Unit(BaseModel):
+    """
+    Справочник для единиц измерения
+    """
+    full_name = models.CharField(
+        max_length=100,
+        verbose_name="Полное наименование"
+    )
+    
+    short_name = models.CharField(
+        max_length=20,
+        blank=True,
+        verbose_name="Сокращённое наименование"
+    )
+    
+    class Meta:
+        verbose_name = "Единица измерения"
+        verbose_name_plural = "Единицы измерения"
+        ordering = ['full_name']
+    
+    def __str__(self):
+        # Возвращаем сокращённое имя, если есть, иначе полное
+        return self.short_name or self.full_name
