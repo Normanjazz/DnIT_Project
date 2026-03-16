cca09aa3b0d0057fe0e054e0823c11f589014994
NormanDev <kulavets@yandex.ru>
2026-03-16 14:20:39 +0400

    feat(directories): добавлена модель WorkType (Виды работ)

--- apps/directories/models.py
+++ apps/directories/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from apps.core.models import BaseModel
 
-# Create your models here.
+
+class WorkType(BaseModel):
+    """
+    Справочник видов работ.    
+    """
+    
+    full_name = models.CharField(
+        max_length=500,
+        verbose_name="Полное наименование"
+    )
+    
+    short_name = models.CharField(
+        max_length=100,
+        blank=True,
+        verbose_name="Сокращённое наименование"
+    )
+    
+    class Meta:
+        verbose_name = "Вид работ"
+        verbose_name_plural = "Виды работ"
+        ordering = ['full_name']
+    
+    def __str__(self):
+        return self.full_name
